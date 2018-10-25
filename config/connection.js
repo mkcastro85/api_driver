f6964c46a308c2396cd63ae9e901cc8f4e56c77d
Ronald Carascal <ronaldcarrascal@gmail.com>
2018-10-24 23:02:40 -0500

    Mejora de datos

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://172.17.0.2:27017/schema_tecnomapsm', (err, res)=>{
+mongoose.connect('mongodb://ds141633.mlab.com:41633/apidriver', (err, res)=>{
     if(err) throw err;
     console.log('established connection');
 });
+mongodb://<dbuser>:<dbpassword>@ds141633.mlab.com:41633/apidriver
 
 /*mongoose.connect('mongodb://tecnomapsm:tecno@ds141960.mlab.com:41960/schema_tecnomapsm', (err, res)=>{
     if(err) throw err;
